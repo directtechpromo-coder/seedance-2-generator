1376cc78ec48b3780c620952db14ab6050255e6c
directtechpromo-coder <directtechpromo@gmail.com>
2026-07-07 22:10:59 +0500

    Update Providers.jsx

--- src/components/Providers.jsx
+++ src/components/Providers.jsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { SessionProvider } from "next-auth/react";
+
 export function Providers({ children }) {
-  return <>{children}</>;
+  return <SessionProvider>{children}</SessionProvider>;
 }
